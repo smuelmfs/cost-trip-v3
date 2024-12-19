c08947473b8566b3190807ea05433eda0381ee7f
smuelmfs <smuelmfs76@gmail.com>
2024-12-19 18:15:44 -0300

    log no mongobd pra ver oq ta pegando

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,15 +2,16 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
+    console.log("Tentando conectar ao MongoDB...");
     if (mongoose.connections[0].readyState) {
-      console.log("MongoDB already connected.");
+      console.log("MongoDB já está conectado.");
       return;
     }
     await mongoose.connect(process.env.MONGODB_URI as string, {});
-    console.log("Connected to MongoDB.");
+    console.log("Conectado ao MongoDB com sucesso.");
   } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
-    throw new Error("MongoDB connection failed");
+    console.error("Erro ao conectar ao MongoDB:", err);
+    throw new Error("Falha na conexão com o MongoDB");
   }
 };
 
